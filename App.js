4b2933660a655ce28b510bc0a9ea1da81b9c573b
aswan10 <aswan.peterpan@gmail.com>
2021-11-07 23:39:59 +0800

    belajar position pada RN

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {View,  ScrollView} from "react-native";
 import MateriFlexBox from "./MateriFlexBox";
+import PositionReactNative from "./PositionReactNative";
 import SampleComponent from "./SampleComponent";
 import StylingReactNativeComponent from "./StylingReactNativeComponent";
 
@@ -8,9 +9,10 @@ const App = () => {
   return (
     <View>
       <ScrollView>
-        {/* <SampleComponent />
-        <StylingReactNativeComponent /> */}
+        <SampleComponent />
+        <StylingReactNativeComponent />
         <MateriFlexBox />
+        <PositionReactNative />
       </ScrollView>
     </View>
   );
